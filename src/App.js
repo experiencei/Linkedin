889897fa8721cc20e929dc1a949cb2bb3e639142
experiencei <ayelojahighbee01@gmail.com>
2021-10-21 17:43:24 +0100

    implementing redux

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import './App.css';
 import Feed from './components/feed/Feed';
 import Header from './components/header/Header';
 import Sidebar from './components/sidebar/Sidebar';
+import selectUser from './components/selectUser/selectUser';
+
 
 function App() {
+
+  const user = useSelector(selectUser)
   return (
     <div className="app">
       <Header/>
-      <div className="app__body">
+
+      {user ? (<Login/>) : (<div className="app__body">
         <Sidebar/>
         <Feed/>
-      </div>
+      </div>)}
+      
     </div>
   );
 }

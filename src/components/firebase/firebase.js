238353d157f6df734783e9ecbd6f0e942cc86a7c
experiencei <ayelojahighbee01@gmail.com>
2021-10-21 13:19:09 +0100

    using redux template to start of linkedin clone

---
+++ src/components/firebase/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCXbI4rbGeUrFQEcuQlDZrz6npsXp3GxGY",
+    authDomain: "ayelojahighbee01.firebaseapp.com",
+    databaseURL: "https://ayelojahighbee01-default-rtdb.firebaseio.com",
+    projectId: "ayelojahighbee01",
+    storageBucket: "ayelojahighbee01.appspot.com",
+    messagingSenderId: "1015820972461",
+    appId: "1:1015820972461:web:a7062512d4c2d25177b124"
+  };

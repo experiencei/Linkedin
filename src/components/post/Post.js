c8f2f953c6ac7bebf91e559b20101851f62e3ca5
experiencei <ayelojahighbee01@gmail.com>
2021-10-21 16:19:57 +0100

    using post from sidebar to populate the post component

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -1,5 +1,7 @@
 import { Avatar } from '@material-ui/core';
+import { ChatOutlined, SendOutlined, ShareOutlined, ThumbUpAltOutlined } from '@material-ui/icons';
 import React from 'react'
+import InputOption from '../inputoption/InputOption';
 import "./Post.css";
 
 function Post({ name , description , message , photoUrl }) {
@@ -8,12 +10,18 @@ function Post({ name , description , message , photoUrl }) {
         <div className="post__header">
         <Avatar/>
             <div className="post__info">
-                <h2>Experiment</h2>
-                <p> description</p>
+                <h2>{name}</h2>
+                <p>{description}</p>
             </div>
         </div>
               <div className="post__body">
-                  <p>hello</p>
+                  <p>{message}</p>
+              </div>
+              <div className="post__buttons">
+                  <InputOption Icon={ThumbUpAltOutlined} title="Like" color="gray"/>
+                  <InputOption Icon={ChatOutlined} title="Comment" color="gray"/>
+                  <InputOption Icon={ShareOutlined} title="Share" color="gray"/>
+                  <InputOption Icon={SendOutlined} title="Send" color="gray"/>
               </div>
         </div>
     )

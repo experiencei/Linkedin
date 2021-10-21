e5e4e3128fcd3698c4af65755d3f8a9218d2babb
experiencei <ayelojahighbee01@gmail.com>
2021-10-21 13:30:35 +0100

    using redux template to start of linkedin clone

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,7 +3,19 @@ import React from 'react'
 function Header() {
     return (
         <div className="header">
-            
+            <div className="header__left">
+               <img
+                   src=""
+                   alt=""
+               />
+               <div className="header__search">
+                   <input type="text"/>
+               </div>
+            </div>
+
+            <div className="header__right">
+
+            </div>
         </div>
     )
 }

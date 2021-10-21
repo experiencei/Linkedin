eecf085dba1404fd1a6f26ba2d85686659d22e2c
experiencei <ayelojahighbee01@gmail.com>
2021-10-21 14:24:07 +0100

    working on the sidebar component

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,9 +1,28 @@
+import { Avatar } from '@material-ui/core'
 import React from 'react'
 import "./Sidebar.css"
 function Sidebar() {
     return (
         <div className="sidebar">
-            
+            <div className="sidebar__top">
+                <img
+                    src=""
+                    alt=""
+                />
+                <Avatar className="sidebar__avatar"/>
+                <h2>Experience</h2>
+                <h4></h4>
+            </div>
+            <div className="sidebar__stats">
+                <div className="sidebar__stat">
+                    <p>who viewed you</p>
+                    <p className="sidebar__statNumber">2,689</p>
+                </div>
+                <div className="sidebar__stat">
+                    <p>Views on post</p>
+                    <p className="sidebar__statNumber">2,689</p>
+                </div>
+            </div>
         </div>
     )
 }

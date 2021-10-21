dd676d69410e808074f5193df252fd602e33e579
experiencei <ayelojahighbee01@gmail.com>
2021-10-21 20:03:33 +0100

    working on the widget with react flip move

--- src/components/widget/Widgets.js
+++ src/components/widget/Widgets.js
@@ -1,12 +1,17 @@
-import { Info } from '@material-ui/icons';
+import { FiberManualRecord, Info } from '@material-ui/icons';
 import React from 'react';
 import "./Widgets.css"
 
 function Widgets() {
    const newsArticle = ( heading , subtitle) => {
        <div className="widgets__article">
-           <div className="widgets__articleLeft"></div>
-           <div className="widgets__articleRight"></div>
+           <div className="widgets__articleLeft">
+               <FiberManualRecord/>
+           </div>
+           <div className="widgets__articleRight">
+               <h4>{heading}</h4>
+               <p>{subtitle}</p>
+           </div>
        </div>
    } 
 
@@ -16,6 +21,7 @@ function Widgets() {
                 <h2>LinkedIn News</h2>
                 <Info/>
             </div>
+            {newsArticle("Experience Iq is good and great" , "Top news - 9099 readers")}
         </div>
     )
 }

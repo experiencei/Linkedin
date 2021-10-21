e573dff48f42b8b4a5c4b9414bb5a5bac0859ff4
experiencei <ayelojahighbee01@gmail.com>
2021-10-21 13:48:00 +0100

    working on headeroption

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import "./Header.css"
 import { Search } from '@material-ui/icons'
+import HeaderOption from '../headerOption/HeaderOption'
 function Header() {
     return (
         <div className="header">
             <div className="header__left">
                <img
                    src="https://www.flaticon.com/svg/static/icons/svg/174/174857.svg"
-                   alt=""
+                   alt="logo"
                />
                <div className="header__search">
                    <Search/>
@@ -16,7 +17,8 @@ function Header() {
             </div>
 
             <div className="header__right">
-
+                <HeaderOption/>
+                <HeaderOption/>
             </div>
         </div>
     )
---
+++ src/components/headerOption/HeaderOption.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./HeaderOption.css";
+
+
+function HeaderOption({}) {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default HeaderOption

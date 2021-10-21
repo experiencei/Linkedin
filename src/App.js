e093893539096f727b76ff810ebe635c9ea59af0
experiencei <ayelojahighbee01@gmail.com>
2021-10-21 13:28:10 +0100

    using redux template to start of linkedin clone

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from './components/header/Header';
 
 function App() {
   return (
     <div className="app">
-      <h1>let clone the linkedin</h1>
+      <Header/>
     </div>
   );
 }

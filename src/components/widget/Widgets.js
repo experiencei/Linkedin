b3bd22f34aa661004c615df434784394eee3c2a8
experiencei <ayelojahighbee01@gmail.com>
2021-10-21 20:00:37 +0100

    working on the widget with react flip move

--- src/components/widget/Widgets.js
+++ src/components/widget/Widgets.js
@@ -3,6 +3,13 @@ import React from 'react';
 import "./Widgets.css"
 
 function Widgets() {
+   const newsArticle = ( heading , subtitle) => {
+       <div className="widgets__article">
+           <div className="widgets__articleLeft"></div>
+           <div className="widgets__articleRight"></div>
+       </div>
+   } 
+
     return (
         <div className="widgets">
             <div className="widgets__header">

9d815f310000a9dfdc2d033db6311dd1f986756e
experiencei <ayelojahighbee01@gmail.com>
2021-10-21 20:22:38 +0100

    working on the widget with react flip move

--- src/components/widget/Widgets.js
+++ src/components/widget/Widgets.js
@@ -13,7 +13,7 @@ function Widgets() {
                <p>{subtitle}</p>
            </div>
        </div>
-   } 
+   } ;
 
     return (
         <div className="widgets">
@@ -22,6 +22,14 @@ function Widgets() {
                 <Info/>
             </div>
             {newsArticle("Experience Iq is good and great" , "Top news - 9099 readers")}
+            {newsArticle("Experience Iq has a shares in Tesla and Alphabets company" , "Top news - 509 readers")}
+            {newsArticle("Covid 19 has negative Effect on football" , "Top news - 59 readers")}
+            {newsArticle("Bitcoin set to hit highest peaks" , "Top news - 598 readers")}
+            {newsArticle("Australia is the first country ..." , "Top news - 259 readers")}
+            {newsArticle("Nigeria is full with a lot great people.." , "Top news - 459 readers")}
+            {newsArticle("Nigerians prays for good governance" , "Top news - 59 readers")}
+            {newsArticle("Redux overtook Context API" , "Top news - 1059 readers")}
+            {newsArticle("Bitcoin breaks $45K.." , "Top news - 4059 readers")}
         </div>
     )
 }
